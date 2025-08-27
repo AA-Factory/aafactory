5ab9951f22985372e79ca8124f87d5bb52e4b437
Reekomer <jeremy.degail@gmail.com>
2025-08-27 13:29:50 +0200

    [Chore] Add docker runpod settings

--- backend/src/start.sh
+++ backend/src/start.sh
@@ -5,16 +5,25 @@ TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
 export LD_PRELOAD="${TCMALLOC}"
 
 # Serve the API and don't shutdown the container
-if [ "$SERVE_API_LOCALLY" == "true" ]; then
-    echo "runpod-worker-comfy: Starting ComfyUI DEV"
-    poetry run python3 /comfyui/main.py --disable-auto-launch --disable-metadata --listen --cpu &
 
-    echo "runpod-worker-comfy: Starting RunPod Handler DEV"
-    poetry run python3 -u /rp_handler.py --rp_serve_api --rp_api_host=0.0.0.0
-else
-    echo "runpod-worker-comfy: Starting ComfyUI PROD"
-    poetry run python3 /comfyui/main.py --disable-auto-launch --disable-metadata &
+echo "runpod-worker-comfy: Starting ComfyUI PROD"
+echo "JUPYTER_PASSWORD: $JUPYTER_PASSWORD"
 
-    echo "runpod-worker-comfy: Starting RunPod Handler PROD"
-    poetry run python3 -u /rp_handler.py
-fi
+# Start jupyter lab
+echo "Starting Jupyter Lab..."
+mkdir -p /workspace && \
+cd / && \
+nohup python3 -m jupyter lab --allow-root --no-browser --port=8888 --ip=* --FileContentsManager.delete_to_trash=False --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' --ServerApp.token=$JUPYTER_PASSWORD --ServerApp.allow_origin=* --ServerApp.preferred_dir=/workspace &> /jupyter.log &
+echo "Jupyter Lab started"
+
+# Download models
+huggingface-cli download LeonJoe13/Sonic --local-dir  ./models/sonic
+huggingface-cli download stabilityai/stable-video-diffusion-img2vid-xt --local-dir  ./models/sonic/stable-video-diffusion-img2vid-xt
+huggingface-cli download openai/whisper-tiny --local-dir ./models/sonic/whisper-tiny
+mv models/sonic/stable-video-diffusion-img2vid-xt/svd_xt.safetensors models/checkpoints/
+mv models/sonic/Sonic/* models/sonic/
+
+poetry run python3 /comfyui/main.py --disable-auto-launch --disable-metadata &
+
+echo "runpod-worker-comfy: Starting RunPod Handler PROD"
+python3 -u /rp_handler.py
