963d73b9d2c9bd75ac0bf97e54fb34f68cb1c5fc
Reekomer <jeremy.degail@gmail.com>
2025-09-06 22:05:26 +0200

    [Fix] Dependencies for video workflow

--- backend/src/start.sh
+++ backend/src/start.sh
@@ -13,7 +13,7 @@ echo "JUPYTER_PASSWORD: $JUPYTER_PASSWORD"
 echo "Starting Jupyter Lab..."
 mkdir -p /workspace && \
 cd / && \
-nohup python3 -m jupyter lab --allow-root --no-browser --port=8888 --ip=* --FileContentsManager.delete_to_trash=False --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' --ServerApp.token=$JUPYTER_PASSWORD --ServerApp.allow_origin=* --ServerApp.preferred_dir=/workspace &> /jupyter.log &
+nohup uv run python -m jupyter lab --allow-root --no-browser --port=8888 --ip=* --FileContentsManager.delete_to_trash=False --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' --ServerApp.token=$JUPYTER_PASSWORD --ServerApp.allow_origin=* --ServerApp.preferred_dir=/workspace &> /jupyter.log &
 echo "Jupyter Lab started"
 
 # Download models
@@ -24,7 +24,7 @@ echo "Jupyter Lab started"
 # mv models/sonic/stable-video-diffusion-img2vid-xt/svd_xt.safetensors models/checkpoints/
 # mv models/sonic/Sonic/* models/sonic/
 
-python3 /comfyui/main.py --disable-auto-launch --disable-metadata --listen &
+uv run python /comfyui/main.py --disable-auto-launch --disable-metadata --listen &
 
 echo "runpod-worker-comfy: Starting RunPod Handler PROD"
-python3 -u /rp_handler.py --rp_serve_api --rp_api_host=0.0.0.0
+uv run python -u /rp_handler.py --rp_serve_api --rp_api_host=0.0.0.0
