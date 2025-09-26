8eea56c939892a4cdeaa14699be59c7c2e9e7494
Reekomer <jeremy.degail@gmail.com>
2025-09-26 19:03:38 +0200

    [Chore] Move model download from entrypoint to Docker Build

--- backend/servers/infinite_talk/entrypoint.sh
+++ backend/servers/infinite_talk/entrypoint.sh
@@ -2,14 +2,15 @@
 set -e
 
 
-uv run huggingface-cli download Wan-AI/Wan2.1-I2V-14B-480P --local-dir ./weights/Wan2.1-I2V-14B-480P &
-uv run huggingface-cli download TencentGameMate/chinese-wav2vec2-base --local-dir ./weights/chinese-wav2vec2-base & 
-uv run huggingface-cli download TencentGameMate/chinese-wav2vec2-base model.safetensors --revision refs/pr/1 --local-dir ./weights/chinese-wav2vec2-base & 
-uv run huggingface-cli download MeiGen-AI/InfiniteTalk --local-dir ./weights/InfiniteTalk &
+uv run hf download Wan-AI/Wan2.1-I2V-14B-480P --local-dir ./weights/Wan2.1-I2V-14B-480P &
+uv run hf download TencentGameMate/chinese-wav2vec2-base --local-dir ./weights/chinese-wav2vec2-base & 
+uv run hf download TencentGameMate/chinese-wav2vec2-base model.safetensors --revision refs/pr/1 --local-dir ./weights/chinese-wav2vec2-base & 
+uv run hf download MeiGen-AI/InfiniteTalk --local-dir ./weights/InfiniteTalk &
+
+# Wait for all background downloads to finish
+wait
+
 # Start redis-server in the background
 redis-server --protected-mode no &
 
-uv run celery -A celery_worker.app worker --loglevel=info -Q infinite_talk -P solo &
-
-# Start the Python app
-uv run uvicorn --host 0.0.0.0 --port 8001 main:app
+uv run celery -A celery_worker.app worker --loglevel=info -Q infinite_talk -P solo
